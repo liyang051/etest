56f809214668be9c7f960aa12a6670cabf205a89
Leone <liyang051@126.com>
2015-01-19 09:55:49 +0800

    update to show friends

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,10 +1,10 @@
 import DS from 'ember-data';
-import Ember from 'ember';
+//import Ember from 'ember';
 
 export default DS.ActiveModelAdapter.extend({
-	namespace: 'api',
-	host: 'http://192.168.1.141:8081',
+	namespace: 'api'
+/*	host: 'http://192.168.1.141:8081',
 	pathForType: function(type) {
 		return Ember.String.underscore(type);
-	}
+	}*/
 });
--- app/routes/friends.js
+++ app/routes/friends.js
@@ -3,7 +3,8 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 	model: function() {
 		var store = this.store;
-		var data = store.find('friends');
+		var data = store.find('friend');
 		console.log("this results =======", data);
+		return data;
 	}
 });
